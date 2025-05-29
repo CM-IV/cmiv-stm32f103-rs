08eda9b0be81c6bf8428acea5ab2312616d64eaa
CM-IV <cmathIV@protonmail.com>
2025-05-29 06:55:01 -0500

    put lcd commands in main loop

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,6 @@
 #![no_main]
 #![no_std]
 
-use defmt::{error, info};
-use defmt_rtt as _;
 use embassy_executor::Spawner;
 use embassy_stm32::{
     gpio::{Level, Output, Speed},
@@ -15,58 +13,66 @@ use embassy_time::{Delay, Timer};
 use i2c_character_display::{CharacterDisplayPCF8574T, LcdDisplayType};
 use panic_probe as _;
 
-#[derive(Debug, defmt::Format)]
-enum LcdError {
-    CommandFailed,
-    InitFailed,
+#[derive(Debug)]
+enum AppError {
+    LcdCommandFailed,
+    LcdInitFailed,
 }
 
 #[embassy_executor::main]
 async fn main(_spawner: Spawner) {
-    info!("Init");
-
     let p = embassy_stm32::init(Default::default());
 
-    let mut led = Output::new(p.PA6, Level::Low, Speed::Low);
+    let mut led = Output::new(p.PA7, Level::Low, Speed::Low);
 
     let i2c = I2c::new_blocking(p.I2C2, p.PB10, p.PB11, Hertz(50_000), Default::default());
 
-    match begin_lcd_commands(i2c).await {
-        Ok(_) => info!("LCD initialized successfully"),
-        Err(e) => {
-            error!("LCD error: {:?}", e);
-            loop {
-                led.set_high();
-                Timer::after_millis(1000).await;
-                led.set_low();
-                Timer::after_millis(1000).await;
-            }
-        }
-    }
-
-    loop {
-        info!("Running!");
-        Timer::after_millis(3000).await;
+    if run_main_loop(i2c).await.is_err() {
+        blink_error_led(&mut led).await;
     }
 }
 
-async fn begin_lcd_commands(i2c: I2c<'static, Blocking>) -> Result<(), LcdError> {
+async fn run_main_loop(i2c: I2c<'static, Blocking>) -> Result<(), AppError> {
     let mut lcd =
         CharacterDisplayPCF8574T::new_with_address(i2c, 0x27, LcdDisplayType::Lcd16x2, Delay);
 
-    lcd.init().map_err(|_| {
-        error!("LCD init failed");
-        LcdError::InitFailed
-    })?;
+    lcd.init().map_err(|_| AppError::LcdInitFailed)?;
 
-    // Commands go in here vvv
-    lcd.backlight(true).map_err(|_| LcdError::CommandFailed)?;
-    lcd.clear().map_err(|_| LcdError::CommandFailed)?;
-    lcd.home().map_err(|_| LcdError::CommandFailed)?;
-    lcd.print("Hello Charlie!")
-        .map_err(|_| LcdError::CommandFailed)?;
-    lcd.blink_cursor(true)
-        .map_err(|_| LcdError::CommandFailed)?;
+    loop {
+        lcd.backlight(true)
+            .map_err(|_| AppError::LcdCommandFailed)?;
+        lcd.clear().map_err(|_| AppError::LcdCommandFailed)?;
+        lcd.home().map_err(|_| AppError::LcdCommandFailed)?;
+        lcd.print("Hello Charlie...")
+            .map_err(|_| AppError::LcdCommandFailed)?;
+        lcd.set_cursor(0, 1)
+            .map_err(|_| AppError::LcdCommandFailed)?;
+        lcd.print("Test ").map_err(|_| AppError::LcdCommandFailed)?;
+        lcd.print(itoa::Buffer::new().format(123))
+            .map_err(|_| AppError::LcdCommandFailed)?;
 
-    Ok(())
+        Timer::after_secs(4).await;
+
+        lcd.clear().map_err(|_| AppError::LcdCommandFailed)?;
+        lcd.home().map_err(|_| AppError::LcdCommandFailed)?;
+        lcd.print("Second screen")
+            .map_err(|_| AppError::LcdCommandFailed)?;
+        lcd.set_cursor(0, 1)
+            .map_err(|_| AppError::LcdCommandFailed)?;
+        lcd.print("Counter: ")
+            .map_err(|_| AppError::LcdCommandFailed)?;
+        lcd.print(itoa::Buffer::new().format(456))
+            .map_err(|_| AppError::LcdCommandFailed)?;
+
+        Timer::after_secs(4).await;
+    }
+}
+
+async fn blink_error_led(led: &mut Output<'static>) -> ! {
+    loop {
+        led.set_high();
+        Timer::after_millis(1000).await;
+        led.set_low();
+        Timer::after_millis(1000).await;
+    }
 }
